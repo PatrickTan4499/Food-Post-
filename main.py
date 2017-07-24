5a7c9ec53780cfbb4e9aee2cc37591c2a6a4c665
winnie3269 <wang.winnie26@gmail.com>
2017-07-24 12:20:22 -0700

    added buttons to home.html

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ import jinja2
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("templates/home.html")
         self.response.write(template.render())
-
-
+    
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
