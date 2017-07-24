7d058d13b5c5e714ca90b3bfc8d39b0b61ccee87
Patrick Tan <patrick4499@gmail.com>
2017-07-24 16:43:07 -0700

    able to store users in database and created classes

--- main.py
+++ main.py
@@ -2,19 +2,41 @@ import webapp2
 import os
 import jinja2
 
+from google.appengine.ext import ndb
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class Donor(ndb.Model):
+    name = ndb.StringProperty()
+    address = ndb.StringProperty()
+    city = ndb.StringProperty()
+    zipcode = ndb.StringProperty()
+    phone = ndb.StringProperty()
+    email = ndb.StringProperty()
 
+class Bank(ndb.Model):
+    name = ndb.StringProperty()
+    address = ndb.StringProperty()
+    city = ndb.StringProperty()
+    zipcode = ndb.StringProperty()
+    phone = ndb.StringProperty()
+    email = ndb.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("templates/home.html")
         self.response.write(template.render())
 
-class FormHandler(webapp2.RequestHandler):
+class DonorFormHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_environment.get_template("templates/form.html")
+        self.response.write(template.render())
+
+    def post(self):
+        donor = Donor(name = self.request.get("name"), city = self.request.get("city"), address = self.request.get("streetname"), zipcode = self.request.get("zipcode"), phone = self.request.get("phone"), email = self.request.get("email") )
+        donor.put()
+        self.redirect('/')
         template = jinja_environment.get_template("templates/form.html")
         self.response.write(template.render())
 
@@ -27,7 +49,7 @@ class ResultHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/form', FormHandler),
+    ('/form', DonorFormHandler),
     ('/result', ResultHandler),
 
 ], debug=True)
