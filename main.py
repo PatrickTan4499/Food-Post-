012220ef961a6177fc9c28eda73bc6596ce6e0ef
winnie3269 <wang.winnie26@gmail.com>
2017-07-24 12:24:06 -0700

    nothing

--- main.py
+++ main.py
@@ -11,22 +11,6 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 
 
-class FormHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template("templates/form.html")
-        self.response.write(template.render())
-
-class ResultHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template("templates/result.html")
-        self.response.write(template.render())
-
-
-
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/form', FormHandler),
-    ('/result', ResultHandler),
-    ('/Map', MapHandler),
-    ('/camera', CameraHandler),
+    ('/', MainHandler)
 ], debug=True)
