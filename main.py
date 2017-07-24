3bab1d0017c85e46b33fc5002bde7206d3998f90
nomaam1000 <nomaam1000@gmail.com>
2017-07-24 12:28:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ import jinja2
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("templates/home.html")
         self.response.write(template.render())
 
+class FormHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template("templates/form.html")
+        self.response.write(template.render())
+
+class ResultHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template("templates/result.html")
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/form', FormHandler),
+    ('/result', ResultHandler),
+    ('/Map', MapHandler),
+    ('/camera', CameraHandler),
 ], debug=True)
