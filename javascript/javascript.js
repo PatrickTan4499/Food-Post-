0952bf8663f6b941f1622557cd01fa9364d896bf
Patrick Tan <patrick4499@gmail.com>
2017-07-25 13:25:01 -0700

    added drop down menu and comments to home

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -17,3 +17,23 @@ if (auth2.isSignedIn.get()) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
 }
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
