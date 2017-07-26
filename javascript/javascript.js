478afe80c8f9b35afd9da4a50fc308e2b778e4a8
Patrick Tan <patrick4499@gmail.com>
2017-07-26 10:39:40 -0700

    fixed dropdown menu

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,3 @@
-//Google sign-in API
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
 
 
 gapi.load('auth2', function() {
@@ -22,7 +14,6 @@ function myFunction() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
