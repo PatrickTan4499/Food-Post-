daa47c412a9b79d5156d570480d39e53dc6bbc44
winnie3269 <wang.winnie26@gmail.com>
2017-07-27 09:58:07 -0700

    created locations for food banks on map

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -26,3 +26,59 @@ window.onclick = function(event) {
     }
   }
 }
+
+var map, infoWindow, service;
+function initMap() {
+  infoWindow = new google.maps.InfoWindow;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 10
+      });
+      map.setCenter(pos);
+      searchForFoodBanks(map);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+// TODO: handle this error by showing a default map
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+function searchForFoodBanks(map) {
+  var request = {
+    location: map.getCenter(),
+    radius: '500',
+    keyword: ['food bank']
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      new google.maps.Marker({
+         map: map,
+         title: place.name,
+         position: place.geometry.location
+        })
+    }
+  }
+}
